11794f416c292cd44f5fd7e95309c1970bf69616
fh <fh@f92f5f20-22a4-4ce4-8604-652ca1bbe0c4>
2009-05-02 10:46:46 +0000

    -back to a flatter directory layout (inspired by the layout of the kdepim project)
    git-svn-id: http://mrburns/svn/dontpanic/trunk@20 f92f5f20-22a4-4ce4-8604-652ca1bbe0c4

--- src/dontpanic.cpp
+++ src/dontpanic.cpp
@@ -3,8 +3,8 @@
  *
  * Copyright (C) 2004 Frank Habel <frank@bugplasma.de>
  */
-#include <dontpanicshell/dontpanic.h>
-#include <dontpanicshell/dontpanic.moc>
+#include "dontpanic.h"
+#include "dontpanic.moc"
 
 #include <kaction.h>
 #include <kactioncollection.h>
