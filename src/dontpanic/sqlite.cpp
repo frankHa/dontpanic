b1d6a6d75d6e2bc8caa1134f463cb955a752dbce
Frank Habel <fh@smeagol.simpsons.nil>
2009-05-13 00:26:05 +0200

    -dontpanic is opening an sqlite database in ~/.dontpanic/dontpanic.sqlite now on startup

--- src/dontpanic/sqlite.cpp
+++ src/dontpanic/sqlite.cpp
@@ -1,13 +1,59 @@
 #include "sqlite.hpp"
+//Qt includes
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QDir>
+// ---------------------------------------------------------------------------------
+
+#define DB_FILE_NAME  "dontpanic.sqlite"
 // ---------------------------------------------------------------------------------
 namespace dp
 {
   // ---------------------------------------------------------------------------------
+  namespace detail
+  {
+    // ---------------------------------------------------------------------------------
+    QString home_dir()
+    {
+      return QDir::homePath() + "/.dontpanic/";
+    }
+    // ---------------------------------------------------------------------------------
+    success prepare_home_directory()
+    {
+      QDir _dir ( home_dir() );
+      if ( _dir.exists() )
+      {
+        return successful();
+      }
+      if ( _dir.mkpath ( home_dir() ) )
+      {
+        return successful();
+      }
+      return error();
+    }
+    // ---------------------------------------------------------------------------------
+    QString database_name()
+    {
+      return home_dir() + DB_FILE_NAME;
+    }
+    // ---------------------------------------------------------------------------------
+  }
+  // ---------------------------------------------------------------------------------
   success Sqlite::open_database_connection()
   {
+    if ( detail::prepare_home_directory().has_failed() )
+    {
+      return error();
+    }
+    QSqlDatabase db = QSqlDatabase::addDatabase ( "QSQLITE" );
+    db.setDatabaseName ( detail::database_name() );
+    if ( db.open() )
+    {
+      return successful();
+    }
     return error();
   }
 
-  // ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
 }//dp
 // ---------------------------------------------------------------------------------
