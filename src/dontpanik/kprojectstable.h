fa4f91ff79f18b547c18921d2bf3b4234f476e8e
Frank Habel <fh@smeagol.simpsons.nil>
2009-12-19 21:14:00 +0100

    -added projects table

---
+++ src/dontpanik/kprojectstable.h
@@ -0,0 +1,53 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef KPROJECTSTABLE_H
+#define KPROJECTSTABLE_H
+
+#include <QTableView>
+// ---------------------------------------------------------------------------------
+namespace dp
+{
+  // ---------------------------------------------------------------------------------
+  namespace detail
+  {
+    // ---------------------------------------------------------------------------------
+    class KProjectsTableModel;
+    // ---------------------------------------------------------------------------------
+  }
+  // ---------------------------------------------------------------------------------
+  class KProjectsTable : public QTableView
+  {
+      // ---------------------------------------------------------------------------------
+      Q_OBJECT
+      // ---------------------------------------------------------------------------------
+    public:
+      // ---------------------------------------------------------------------------------
+      KProjectsTable ( QWidget* parent = 0 );
+      // ---------------------------------------------------------------------------------
+    private:
+      // ---------------------------------------------------------------------------------
+      detail::KProjectsTableModel *_M_model;
+      // ---------------------------------------------------------------------------------
+  };
+  // ---------------------------------------------------------------------------------
+}//dp
+// ---------------------------------------------------------------------------------
+
+#endif // KPROJECTSTABLE_H
---
+++ src/dontpanik/kprojectstablemodel.h
@@ -0,0 +1,66 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef KPROJECTSTABLEMODEL_H
+#define KPROJECTSTABLEMODEL_H
+
+#include <QAbstractTableModel>
+#include <QStringList>
+
+// ---------------------------------------------------------------------------------
+namespace dp
+{
+  // ---------------------------------------------------------------------------------
+  namespace detail
+  {
+    // ---------------------------------------------------------------------------------
+    class KProjectsTableModel : public QAbstractTableModel
+    {
+        // ---------------------------------------------------------------------------------
+        Q_OBJECT
+        // ---------------------------------------------------------------------------------
+      public:
+        // ---------------------------------------------------------------------------------
+        KProjectsTableModel ( QObject* parent = 0 );
+        // ---------------------------------------------------------------------------------
+      public:
+        // ---------------------------------------------------------------------------------
+        virtual QVariant data ( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+        // ---------------------------------------------------------------------------------
+        virtual QVariant headerData ( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+        // ---------------------------------------------------------------------------------
+        virtual int columnCount ( const QModelIndex& parent = QModelIndex() ) const;
+        // ---------------------------------------------------------------------------------
+        virtual int rowCount ( const QModelIndex& parent = QModelIndex() ) const;
+        // ---------------------------------------------------------------------------------
+      private:
+        // ---------------------------------------------------------------------------------
+        void init_header_data();
+        // ---------------------------------------------------------------------------------
+      private:
+        // ---------------------------------------------------------------------------------
+        QStringList _M_headers;
+        // ---------------------------------------------------------------------------------
+    };
+    // ---------------------------------------------------------------------------------
+  }//detail
+  // ---------------------------------------------------------------------------------
+}//dp
+// ---------------------------------------------------------------------------------
+#endif // KPROJECTSTABLEMODEL_H
