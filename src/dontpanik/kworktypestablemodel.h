9f773636881bd512d0ce8545459192d6cb0a20f8
Frank Habel <fh@smeagol.simpsons.nil>
2009-12-22 19:24:08 +0100

    -added KWorkTypesTable

---
+++ src/dontpanik/kworktypestablemodel.h
@@ -0,0 +1,71 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef KWORKTYPESTABLEMODEL_H
+#define KWORKTYPESTABLEMODEL_H
+
+#include <QAbstractTableModel>
+#include <QStringList>
+
+// ---------------------------------------------------------------------------------
+namespace dp
+{
+  // ---------------------------------------------------------------------------------
+  namespace core
+  {
+    // ---------------------------------------------------------------------------------
+    namespace detail
+    {
+      // ---------------------------------------------------------------------------------
+      class KWorkTypesTableModel : public QAbstractTableModel
+      {
+          // ---------------------------------------------------------------------------------
+          Q_OBJECT
+          // ---------------------------------------------------------------------------------
+        public:
+          // ---------------------------------------------------------------------------------
+          KWorkTypesTableModel ( QObject* parent = 0 );
+          // ---------------------------------------------------------------------------------
+        public:
+          // ---------------------------------------------------------------------------------
+          virtual QVariant data ( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+          // ---------------------------------------------------------------------------------
+          virtual QVariant headerData ( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+          // ---------------------------------------------------------------------------------
+          virtual int columnCount ( const QModelIndex& parent = QModelIndex() ) const;
+          // ---------------------------------------------------------------------------------
+          virtual int rowCount ( const QModelIndex& parent = QModelIndex() ) const;
+          // ---------------------------------------------------------------------------------
+        private:
+          // ---------------------------------------------------------------------------------
+          void init_header_data();
+          // ---------------------------------------------------------------------------------
+        private:
+          // ---------------------------------------------------------------------------------
+          QStringList _M_headers;
+          // ---------------------------------------------------------------------------------
+      };
+      // ---------------------------------------------------------------------------------
+    }//detail
+    // ---------------------------------------------------------------------------------
+  }
+  // ---------------------------------------------------------------------------------
+}//dp
+// ---------------------------------------------------------------------------------
+#endif // KWORKTYPESTABLEMODEL_H
