ab1da1d02ac712d081d0dacb5b155fb7b56d3cab
Frank Habel <fh@smeagol.simpsons.nil>
2010-01-20 21:46:33 +0100

    -added stubs for a KActionTemplatesList

---
+++ src/dontpanik/kpart/core/kactiontemplateslistmodel.cpp
@@ -0,0 +1,147 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "kactiontemplateslistmodel.h"
+#include "context.h"
+#include <KLocalizedString>
+// ---------------------------------------------------------------------------------
+namespace dp
+{
+  // ---------------------------------------------------------------------------------
+  namespace core
+  {
+    // ---------------------------------------------------------------------------------
+    namespace detail
+    {
+      // ---------------------------------------------------------------------------------
+      enum{NAME, DATE};
+      // ---------------------------------------------------------------------------------
+      KActionTemplatesListModel::KActionTemplatesListModel ( QObject *parent )
+          : QAbstractListModel ( parent )
+      {
+        init_header_data();
+	subscribe_to_project_manager_signals();
+        init_projects_list();	
+      }
+      // ---------------------------------------------------------------------------------
+      QVariant KActionTemplatesListModel::data ( const QModelIndex& index, int role ) const
+      {
+	if(!index.isValid())return QVariant();
+	if(role != Qt::DisplayRole) return QVariant();
+	Project const& p = _M_projects.at(index.row());
+	switch(index.column())
+	{
+	  case NAME: return p.name();
+	  case DATE: return p.creationDate();
+	  default: return QVariant();
+	}
+      }
+      // ---------------------------------------------------------------------------------
+      QVariant KActionTemplatesListModel::headerData ( int section, Qt::Orientation orientation, int role ) const
+      {
+        if ( role != Qt::DisplayRole )
+        {
+          return QVariant();
+        }
+        if ( orientation != Qt::Horizontal )
+        {
+          return QVariant();
+        }
+        return _M_headers.at ( section );
+      }
+      // ---------------------------------------------------------------------------------
+      int KActionTemplatesListModel::columnCount ( const QModelIndex& parent ) const
+      {
+        return _M_headers.count();
+      }
+      // ---------------------------------------------------------------------------------
+      int KActionTemplatesListModel::rowCount ( const QModelIndex& parent ) const
+      {
+        return _M_projects.size();
+      }
+      // ---------------------------------------------------------------------------------
+      // private stuff:
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::init_header_data()
+      {
+        _M_headers << i18n ( "Name" )
+        << i18n ( "Creation Date" );
+      }
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::init_projects_list()
+      {
+        _M_projects = context()->projectManager()->allProjects();
+      }
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::subscribe_to_project_manager_signals()
+      {
+	client::ProjectManager * pm = context()->projectManager();
+        connect(pm, SIGNAL(stored(dp::Project)), this, SLOT(stored(dp::Project const&)));
+	connect(pm, SIGNAL(removed(dp::Project)), this, SLOT(removed(dp::Project const&)));
+      }
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::stored(dp::Project const&p)
+      {
+	qWarning()<<__FUNCTION__;
+	if(is_already_known(p))
+	{
+	  updated(p);
+	}
+	else
+	{
+	  added(p);
+	}
+      }
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::removed(dp::Project const&p)
+      {
+	int i = _M_projects.indexOf(p);
+	beginRemoveRows(QModelIndex(), i, i);
+	_M_projects.removeAt(i);
+	endRemoveRows();
+      }
+      // ---------------------------------------------------------------------------------
+      bool KActionTemplatesListModel::is_already_known(dp::Project const&p) const
+      {
+	return (_M_projects.indexOf(p)!=-1);
+      }	  
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::added(dp::Project const&p)
+      {
+	int index = _M_projects.size();
+	beginInsertRows(QModelIndex(), index, index);
+	_M_projects.append(p);
+	endInsertRows();
+      }	  
+      // ---------------------------------------------------------------------------------
+      void KActionTemplatesListModel::updated(dp::Project const&p)
+      {
+	int row = _M_projects.indexOf(p);
+	QModelIndex const& i = index(row, NAME);
+	dp::Project _p = _M_projects.at(row);
+	_p.setName(p.name());
+	emit dataChanged(i, i);
+      }	  
+      // ---------------------------------------------------------------------------------
+    }//detail
+    // ---------------------------------------------------------------------------------
+  }//core
+  // ---------------------------------------------------------------------------------
+}//dp
+// ---------------------------------------------------------------------------------
---
+++ src/dontpanik/kpart/core/kactiontemplateslistmodel.h
@@ -0,0 +1,90 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef KACTIONTEMPLATESLISTMODEL_H
+#define KACTIONTEMPLATESLISTMODEL_H
+
+#include <QAbstractListModel>
+#include <QStringList>
+#include <libdontpanic/project.hpp>
+
+// ---------------------------------------------------------------------------------
+namespace dp
+{
+  // ---------------------------------------------------------------------------------
+  namespace core
+  {
+    // ---------------------------------------------------------------------------------
+    namespace detail
+    {
+      // ---------------------------------------------------------------------------------
+      class KActionTemplatesListModel: public QAbstractListModel
+      {
+          // ---------------------------------------------------------------------------------
+          Q_OBJECT
+          // ---------------------------------------------------------------------------------
+        public:
+          // ---------------------------------------------------------------------------------
+          KActionTemplatesListModel ( QObject* parent = 0 );
+          // ---------------------------------------------------------------------------------
+	private slots:
+	  // ---------------------------------------------------------------------------------
+	  void stored(dp::Project const&);
+	  // ---------------------------------------------------------------------------------
+	  void removed(dp::Project const&);
+	  // ---------------------------------------------------------------------------------
+        public:
+          // ---------------------------------------------------------------------------------
+          virtual QVariant data ( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+          // ---------------------------------------------------------------------------------
+          virtual QVariant headerData ( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
+          // ---------------------------------------------------------------------------------
+          virtual int columnCount ( const QModelIndex& parent = QModelIndex() ) const;
+          // ---------------------------------------------------------------------------------
+          virtual int rowCount ( const QModelIndex& parent = QModelIndex() ) const;
+          // ---------------------------------------------------------------------------------
+        private:
+          // ---------------------------------------------------------------------------------
+          void init_header_data();
+          // ---------------------------------------------------------------------------------
+          void init_projects_list();
+	  // ---------------------------------------------------------------------------------
+	  void subscribe_to_project_manager_signals();
+          // ---------------------------------------------------------------------------------
+	  bool is_already_known(dp::Project const&) const;
+	  // ---------------------------------------------------------------------------------
+	  void added(dp::Project const&);
+	  // ---------------------------------------------------------------------------------
+	  void updated(dp::Project const&);
+	  // ---------------------------------------------------------------------------------
+        private:
+          // ---------------------------------------------------------------------------------
+          QStringList _M_headers;
+	  // ---------------------------------------------------------------------------------
+	  ProjectList _M_projects;
+          // ---------------------------------------------------------------------------------
+      };
+      // ---------------------------------------------------------------------------------
+    }//detail
+    // ---------------------------------------------------------------------------------
+  }
+  // ---------------------------------------------------------------------------------
+}//dp
+// ---------------------------------------------------------------------------------
+#endif // KACTIONTEMPLATESLISTMODEL_H
