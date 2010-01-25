6438388e66decd78b1bbc4667a2a995befbf63b8
Frank Habel <fh@smeagol.simpsons.nil>
2010-01-25 09:59:49 +0100

    -removed DP_EXPORT stuff for testing

--- src/include/libdontpanic/dp_export.hpp
+++ src/include/libdontpanic/dp_export.hpp
@@ -3,14 +3,15 @@
 /* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 // ---------------------------------------------------------------------------------
-#ifndef DP_EXPORT
-# if defined(MAKE_DPPRIVATE_LIB)
-   /* We are building this library */
-#  define DP_EXPORT KDE_EXPORT
-# else
-   /* We are using this library */
-#  define DP_EXPORT KDE_IMPORT
-# endif
-#endif
+// #ifndef DP_EXPORT
+// # if defined(MAKE_DPPRIVATE_LIB)
+//    /* We are building this library */
+// #  define DP_EXPORT KDE_EXPORT
+// # else
+//    /* We are using this library */
+// #  define DP_EXPORT KDE_IMPORT
+// # endif
+// #endif
+#define DP_EXPORT
 // ---------------------------------------------------------------------------------
 #endif //DP_EXPORT_HPP
