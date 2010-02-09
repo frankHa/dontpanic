084201022ebbe8281a47f92e69cc06001eaf28f9
Frank Habel <frank@bugplasma.de>
2010-02-09 23:09:15 +0100

    -assigned some KDE icons to our global actions

--- src/dontpanik/kpart/dontpanik_part.cpp
+++ src/dontpanik/kpart/dontpanik_part.cpp
@@ -118,46 +118,54 @@ void DontPanikPart::setup_actions()
 {
   KAction *edit_projects = new KAction ( this );
   edit_projects->setText ( i18n ( "Edit Projects" ) );
+  edit_projects->setIcon(KIcon("applications-engineering"));
   actionCollection()->addAction ( "edit_projects", edit_projects );
   connect ( edit_projects, SIGNAL ( triggered() ), this, SLOT ( editProjects() ) );
   
   KAction *edit_tasks = new KAction ( this );
   edit_tasks->setText ( i18n ( "Edit Tasks/Worktypes" ) );
+  edit_tasks->setIcon(KIcon("view-pim-tasks"));
   actionCollection()->addAction ( "edit_tasks", edit_tasks );
   connect ( edit_tasks, SIGNAL ( triggered() ), this, SLOT ( editTasks() ) );
   
   KAction *add_action = new KAction(this);
   add_action->setText(i18n("Add Action"));
+  add_action->setIcon(KIcon("document-new"));
   add_action->setShortcut( KShortcut(i18n("Ctrl+A")));
   actionCollection()->addAction ( "add_action", add_action );
   connect ( add_action, SIGNAL ( triggered() ), this, SLOT ( addAction() ) );
   
   KAction *start_new_action = new KAction(this);
   start_new_action->setText(i18n("Start Action"));
+  start_new_action->setIcon(KIcon("media-playback-start"));
   start_new_action->setShortcut( KShortcut(i18n("Ctrl+S")));
   actionCollection()->addAction ( "start_new_action", start_new_action );
   connect ( start_new_action, SIGNAL ( triggered() ), this, SLOT ( startNewAction() ) );
   
   KAction *stop_current_action = new KAction(this);
   stop_current_action->setText(i18n("Stop current Action"));
+  stop_current_action->setIcon(KIcon("media-playback-stop"));
   stop_current_action->setShortcut( KShortcut(i18n("Ctrl+T")));
   actionCollection()->addAction ( "stop_current_action", stop_current_action );
   connect ( stop_current_action, SIGNAL ( triggered() ), this, SLOT ( stopCurrentAction() ) );
   
   KAction *continue_last_action = new KAction(this);
   continue_last_action->setText(i18n("Resume the last Action"));
+  continue_last_action->setIcon(KIcon("media-seek-forward"));
   continue_last_action->setShortcut( KShortcut(i18n("Ctrl+R")));
   actionCollection()->addAction ( "continue_last_action", continue_last_action );
   connect ( continue_last_action, SIGNAL ( triggered() ), this, SLOT ( continueLastAction() ) );
   
   KAction *view_day_action = new KAction(this);
   view_day_action->setText(i18n("Day View"));
+  view_day_action->setIcon(KIcon("view-pim-calendar"));
   view_day_action->setShortcut( KShortcut(i18n("Ctrl+V, Ctrl+D")));
   actionCollection()->addAction ( "view_day", view_day_action );
   connect ( view_day_action, SIGNAL ( triggered() ), this, SLOT ( viewDayAction() ) );
   
   KAction *view_reports_action = new KAction(this);
   view_reports_action->setText(i18n("Reports View"));
+  view_reports_action->setIcon(KIcon("view-calendar-list"));
   view_reports_action->setShortcut( KShortcut(i18n("Ctrl+V, Ctrl+R")));
   actionCollection()->addAction ( "view_reports", view_reports_action );
   connect ( view_reports_action, SIGNAL ( triggered() ), this, SLOT ( viewReportsAction() ) );
