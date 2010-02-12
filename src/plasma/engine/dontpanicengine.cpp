4317ae02d20e464b6cbc761aab6d1218672a9623
Frank Habel <frank@bugplasma.de>
2010-02-12 20:16:53 +0100

    -initializing client versions of the managers in order to  communicate with the dontpanic core

--- src/plasma/engine/dontpanicengine.cpp
+++ src/plasma/engine/dontpanicengine.cpp
@@ -1,23 +1,39 @@
 #include "dontpanicengine.h"
- 
+
 #include <QDate>
 #include <QTime>
- 
+
+#include <libdontpanic_client/actiontemplatemanager.h>
+#include <libdontpanic_client/timetracker.h>
+#include <libdontpanic_client/projectmanager.h>
+#include <libdontpanic_client/taskmanager.h>
 #include <Plasma/DataContainer>
- 
+
+namespace dp
+{
+namespace plasma
+{
 DontPanicEngine::DontPanicEngine(QObject* parent, const QVariantList& args)
-    : Plasma::DataEngine(parent, args)
+        : Plasma::DataEngine(parent, args)
 {
     // We ignore any arguments - data engines do not have much use for them
     Q_UNUSED(args)
- 
+
     // This prevents applets from setting an unnecessarily high
     // update interval and using too much CPU.
     // In the case of a clock that only has second precision,
     // a third of a second should be more than enough.
     setMinimumPollingInterval(333);
 }
- 
+
+void DontPanicEngine::init()
+{
+    _M_action_template_manager = new dp::client::ActionTemplateManager();
+    _M_project_manager = new dp::client::ProjectManager();
+    _M_task_manager = new dp::client::TaskManager();
+    _M_timetracker = new dp::client::TimeTracker();
+}
+
 bool DontPanicEngine::sourceRequestEvent(const QString &name)
 {
     // We do not have any special code to execute the
@@ -25,18 +41,24 @@ bool DontPanicEngine::sourceRequestEvent(const QString &name)
     // updateSourceEvent().
     return updateSourceEvent(name);
 }
- 
+
 bool DontPanicEngine::updateSourceEvent(const QString &name)
 {
     setData(name, I18N_NOOP("Time"), QTime::currentTime());
     setData(name, I18N_NOOP("Date"), QDate::currentDate());
     return true;
 }
- 
+
+QStringList DontPanicEngine::sources() const
+{
+    return QStringList();
+}
+}
+}
 // This does the magic that allows Plasma to load
 // this plugin.  The first argument must match
 // the X-Plasma-EngineName in the .desktop file.
-K_EXPORT_PLASMA_DATAENGINE(dontpanic, DontPanicEngine)
- 
+K_EXPORT_PLASMA_DATAENGINE(dontpanic, dp::plasma::DontPanicEngine)
+
 // this is needed since DontPanicEngine is a QObject
 #include "dontpanicengine.moc"
--- src/plasma/engine/dontpanicengine.h
+++ src/plasma/engine/dontpanicengine.h
@@ -2,34 +2,53 @@
 // header is included multiple times
 #ifndef DONTPANICENGINE_H
 #define DONTPANICENGINE_H
- 
+#include <libdontpanic/defines.hpp>
 // We need the DataEngine header, since we are inheriting it
 #include <Plasma/DataEngine>
- 
-/**
- * This engine provides the current date and time for a given
- * timezone.
- *
- * "Local" is a special source that is an alias for the current
- * timezone.
- */
+
+namespace dp
+{
+namespace client
+{
+class ActionTemplateManager;
+class ProjectManager;
+class TaskManager;
+class TimeTracker;
+}
+namespace plasma
+{
 class DontPanicEngine : public Plasma::DataEngine
 {
     // required since Plasma::DataEngine inherits QObject
     Q_OBJECT
- 
-    public:
-        // every engine needs a constructor with these arguments
-        DontPanicEngine(QObject* parent, const QVariantList& args);
- 
-    protected:
-        // this virtual function is called when a new source is requested
-        bool sourceRequestEvent(const QString& name);
- 
-        // this virtual function is called when an automatic update
-        // is triggered for an existing source (ie: when a valid update
-        // interval is set when requesting a source)
-        bool updateSourceEvent(const QString& source);
+
+public:
+    // every engine needs a constructor with these arguments
+    DontPanicEngine(QObject* parent, const QVariantList& args);
+
+protected:
+    void init();
+    // this virtual function is called when a new source is requested
+    bool sourceRequestEvent(const QString& name);
+
+    // this virtual function is called when an automatic update
+    // is triggered for an existing source (ie: when a valid update
+    // interval is set when requesting a source)
+    bool updateSourceEvent(const QString& source);
+
+    QStringList sources() const;
+
+private:
+    // ---------------------------------------------------------------------------------
+    dp::client::ActionTemplateManager *_M_action_template_manager;
+    // ---------------------------------------------------------------------------------
+    dp::client::ProjectManager *_M_project_manager;
+    // ---------------------------------------------------------------------------------
+    dp::client::TaskManager *_M_task_manager;
+    // ---------------------------------------------------------------------------------
+    dp::client::TimeTracker *_M_timetracker;
+    // ---------------------------------------------------------------------------------
 };
- 
+}
+}
 #endif // DONTPANICENGINE_H
