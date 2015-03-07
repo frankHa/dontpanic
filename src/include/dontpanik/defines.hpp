20fc3ec7678dd6520e7129298e9d45ac7ab0109f
Frank Habel <frank@bugplasma.de>
2015-03-07 12:53:10 +0100

    -added a missing header file

---
+++ src/include/dontpanik/defines.hpp
@@ -0,0 +1,7 @@
+#ifndef DONTPANIK_DEFINES_HPP
+#define DONTPANIK_DEFINES_HPP
+// ---------------------------------------------------------------------------------
+#include <config.h>
+#include <libdontpanic/defines.hpp>
+// ---------------------------------------------------------------------------------
+#endif //DONTPANIK_DEFINES_HPP
