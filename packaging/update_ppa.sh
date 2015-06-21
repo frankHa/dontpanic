c834d6409d8fa123ea19bf5c4c093132231aef65
Frank Habel <frank@bugplasma.de>
2015-06-22 00:27:03 +0200

    added some deb helper scripts

---
+++ packaging/update_ppa.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd src
+version=`dpkg-parsechangelog --show-field Version`
+baseversion=`echo ${version} | sed -n 's/-[0-9]*ppa[0-9]*~.*//p'`
+cd ..
+tar cjf dontpanic_${baseversion}.orig.tar.bz2 --exclude src/debian --exclude src/.kdev4 src
+cd src
+debuild -S -sa
+cd ..
+dput ppa:dp-fh/dontpanic dontpanic_${version}_source.changes
